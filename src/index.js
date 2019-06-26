712c12dfd038e77b785a143342ebef7938ab0785
Alex Wilmer <djazium@gmail.com>
2019-06-26 12:09:37 -0400

    fixerupr

--- src/index.js
+++ src/index.js
@@ -11,26 +11,13 @@ osc.connect(ctx.destination);
 osc.start();
 
 window.onmousemove = event => {
-  let color = process.env.REACT_APP_COLOR;
-
-  if (color) document.body.style.backgroundColor = color;
-  else {
-    document.body.style.backgroundColor = `hsl(${event.clientX}, 60%, 60%)`;
-  }
-
+  document.body.style.backgroundColor = `hsl(${event.clientX}, 60%, 60%)`;
   osc.frequency.value = event.clientX;
 };
 
 render(
   <>
-    <h1>move your mouse around !!!!</h1>
-    <button
-      onClick={() => {
-        console.log(process.env.REACT_APP_COLOR);
-      }}
-    >
-      what's the color
-    </button>
+    <h1>Move your mouse around !!!! (make sure your volume is low)</h1>
   </>,
   document.getElementById("root")
 );
