e0c01c6c94af6ae47a24be861aeaf20ce39a55f2
Alex Wilmer <djazium@gmail.com>
2019-06-26 11:35:58 -0400

    asldkjlas

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ osc.start();
 window.onmousemove = event => {
   let color = process.env.REACT_APP_COLOR;
 
-  document.body.style.backgroundColor = `hsl(${
-    color ? color : event.clientX
-  }, 60%, 60%)`;
+  if (color) document.body.style.backgroundColor = color;
+  else {
+    document.body.style.backgroundColor = `hsl(${event.clientX}, 60%, 60%)`;
+  }
 
   osc.frequency.value = event.clientX;
 };
